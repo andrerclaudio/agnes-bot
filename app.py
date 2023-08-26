2521f75260c767ca2e4e440414cb7b006bb265df
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2023-08-26 16:56:50 -0300

    Added files and others

--- app.py
+++ app.py
@@ -1,73 +1,96 @@
 # Build-in modules
 import logging
-import time
-from datetime import timedelta
-from threading import ThreadError, Thread
+import configparser
+import html
+import json
+import traceback
 
 # Added modules
-from pytictoc import TicToc
+from telegram import ForceReply, Update
+from telegram.constants import ParseMode
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-# Project modules
-
-# Print in file
-# logging.basicConfig(filename='logs.log',
-#                     filemode='w',
-#                     level=logging.INFO,
-#                     format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-#                     datefmt='%d/%b/%Y - %H:%M:%S')
-
-# Print in software terminal
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s | %(process)d | %(name)s | %(levelname)s:  %(message)s',
-                    datefmt='%d/%b/%Y - %H:%M:%S')
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-
-class ElapsedTime(object):
-    """
-    Measure the elapsed time between Tic and Toc
-    """
-    def __init__(self):
-        self.t = TicToc()
-        self.t.tic()
-
-    def elapsed(self):
-        _elapsed = self.t.tocvalue()
-        d = timedelta(seconds=_elapsed)
-        logger.info('< {} >'.format(d))
+config = configparser.ConfigParser()
+config.read_file(open('config.ini'))
+TOKEN = config['DEFAULT']['token']
+CHAT_ID = config['DEFAULT']['error_forward_id']
 
 
-class ThreadingProcessQueue(object):
-    """
-    The run() method will be started and it will run in the background
-    until the application exits.
-    """
-
-    def __init__(self, interval):
-        """
-        Constructor
-        """
-        self.interval = interval
-
-        thread = Thread(target=run, args=(self.interval,), name='Thread_name')
-        thread.daemon = True  # Daemonize thread
-        thread.start()  # Start the execution
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
 
 
-def run(interval):
-    """ Method that runs forever """
-    while True:
-        try:
-            time.sleep(interval)
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
 
-        except ThreadError as e:
-            logger.exception('{}'.format(e))
 
-        finally:
-            pass
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error("Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096-character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(
+        chat_id=CHAT_ID, text=message, parse_mode=ParseMode.HTML
+    )
 
 
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
+
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    app = Application.builder().token(TOKEN).build()
+
+    # on different commands - answer in Telegram
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e. message - echo the message on Telegram
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # ...and the error handler
+    app.add_error_handler(error_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
