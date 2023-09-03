7b4801426fe9c688bfb1e68fc2ef083bcd68604c
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2023-09-03 18:22:55 -0300

    Minor adjusts.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 import configparser
 
 config = configparser.ConfigParser()
-config.read_file(open('config.ini'))
+config.read_file(open('/home/pi/agnes-bot/config.ini'))
 TOKEN = config['DEFAULT']['token']
 CHAT_ID = config['DEFAULT']['error_forward_id']
