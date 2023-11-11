7ccf3e8c425d1958453e747fad8020060bb95df5
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2023-11-10 21:23:29 -0300

    Sending messages using Tlegram Bot for Mqtt msg.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters
 # Application modules
 from config import TOKEN
 from handlers.handlers import *
+from mqtt.mqtt import AgnesMqttClient
 
 # Enable logging
 logging.basicConfig(
@@ -26,32 +27,41 @@ def application():
     # Just a delay doing nothing and making sure the connection was properly established.
     time.sleep(60)
 
-    """Init the class."""
+    """Init the class to control the raspberry pi PIN."""
     finger = DigitalFinger()
+
+    """Init the Mqtt class."""
+    BROKER_ADDRESS = "mqtt.eclipseprojects.io"
+    PORT = 1883
+    client = AgnesMqttClient() 
 
     try:
 
+        """Start Mqtt client."""
+        client.mqttc.connect(BROKER_ADDRESS, PORT)
+        client.mqttc.loop_start()
+
         """Start the bot."""
         # Create the Application and pass it your bot's token.
-        app = Application.builder().token(TOKEN).build()
-    
+        bot = Application.builder().token(TOKEN).build()
+
         # on different commands - answer in Telegram
-        app.add_handler(CommandHandler("start", start))
-        app.add_handler(CommandHandler("help", help_command))
+        bot.add_handler(CommandHandler("start", start))
+        bot.add_handler(CommandHandler("help", help_command))
     
         # Add the new "Asimov" command handler
-        app.add_handler(CommandHandler("Asimov", lambda update, context: asimov_control(update, finger)))
+        bot.add_handler(CommandHandler("Asimov", lambda update, context: asimov_control(update, finger)))
     
         # on non command i.e. message - echo the message on Telegram
-        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo_text))
+        bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo_text))
         # Add a handler for photo messages
-        app.add_handler(MessageHandler(filters.PHOTO, colors))
+        bot.add_handler(MessageHandler(filters.PHOTO, colors))
     
         # ...and the error handler
-        app.add_error_handler(error_handler)
+        bot.add_error_handler(error_handler)
     
         # Run the bot until the user presses Ctrl-C
-        app.run_polling(allowed_updates=Update.ALL_TYPES)
+        bot.run_polling(allowed_updates=Update.ALL_TYPES)
 
     except Exception as e:
         logging.error(f"Something went wrong -- {str(e)}", exc_info=False) 
@@ -59,3 +69,5 @@ def application():
     finally:
         # Clean up GPIO and exit
         finger.clean()
+        client.mqttc.disconnect()
+        logging.info("Disconnected from MQTT broker, Telegram Bot and Reset PIN settings.")
--- mqtt/mqtt.py
+++ mqtt/mqtt.py
@@ -3,15 +3,9 @@ MQTT application in general.
 """
 
 import logging
-import traceback
+import requests
+from config import CHAT_ID, MQTT_CLIENT_ID, MQTT_TOPIC, TOKEN
 import paho.mqtt.client as mqtt
-
-
-# Define the MQTT broker information
-BROKER_ADDRESS = "mqtt.eclipseprojects.io"
-PORT = 1883
-TOPIC = "my_topic"
-CLIENT_ID = "my_client"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
@@ -42,81 +36,102 @@ class MQTTConnectionError(Exception):
         super().__init__(self.message)
 
 
-def on_connect(client, userdata, flags, rc) -> None:
+class AgnesMqttClient(object):
     """
-    MQTT on_connect callback function.
+    AgnesMqttClient class for handling MQTT communication.
 
-    This function is called when the MQTT client successfully connects to the broker
-    or encounters an error during connection.
+    This class encapsulates an MQTT client with specific configurations for the Agnes
+    application. It provides methods and attributes for setting up and interacting
+    with the MQTT broker.
 
-    Args:
-        client (paho.mqtt.client.Client): The MQTT client instance.
-        userdata (Any): User-defined data passed to the client.
-        flags (dict): Flags indicating specific MQTT connection flags.
-        rc (int): The connection result code.
-
-    Returns:
-        None
-
-    Raises:
-        Exception: If the MQTT connection fails (rc is not 0).
+    Attributes:
+        TOPIC (str): The MQTT topic to subscribe to ("agnes.mqtt").
+        CLIENT_ID (str): The client ID for the MQTT broker ("bridge").
+        mqttc (paho.mqtt.client.Client): The MQTT client instance.
 
     Example:
-        This function is typically set as the on_connect callback when configuring
-        an MQTT client. It handles the logic for successful and failed connections.
+        # Create an instance of AgnesMqttClient
+        agnes_client = AgnesMqttClient()
+
+        # Perform further configuration if needed
 
     """
 
-    if rc == 0:
+    def __init__(self) -> None:
+        """
+        Initialize an instance of AgnesMqttClient.
 
+        This constructor sets up the MQTT client with predefined configurations,
+        including the topic and client ID.
+
+        """
+
+        # Define the MQTT broker client ID and Topic to subscribe        
+        self.TOPIC = MQTT_TOPIC
+        self.CLIENT_ID = MQTT_CLIENT_ID
+
+        # MQTT client setup
+        self.mqttc = mqtt.Client(self.CLIENT_ID)
+        self.mqttc.on_connect = self.__on_connect
+        self.mqttc.on_message = self.__on_message
+
+
+    def __on_connect(self, client, userdata, flags, rc) -> None:
+        """
+        MQTT on_connect callback function.
+
+        This function is called when the MQTT client successfully connects to the broker
+        or encounters an error during connection.
+
+        Args:
+            client (paho.mqtt.client.Client): The MQTT client instance.
+            userdata (Any): User-defined data passed to the client.
+            flags (dict): Flags indicating specific MQTT connection flags.
+            rc (int): The connection result code.
+
+        Returns:
+            None
+
+        Raises:
+            Exception: If the MQTT connection fails (rc is not 0).
+
+        Example:
+            This function is typically set as the on_connect callback when configuring
+            an MQTT client. It handles the logic for successful and failed connections.
+
+        """
+
+        if rc == 0:
+
+            _ = client
+            _ = userdata
+            _ = flags
+
+            logging.info("Connected to MQTT broker")
+            client.subscribe(self.TOPIC)
+        else:
+            error_msg = f"Connection failed with code {rc}"
+            logging.error(error_msg)
+            raise MQTTConnectionError(error_msg)
+
+
+    def __on_message(self, client, userdata, message) -> None:
+        """
+        Parse the incoming messages.
+        """
+        
         _ = client
         _ = userdata
-        _ = flags
 
-        logging.info("Connected to MQTT broker")
-        client.subscribe(TOPIC)
-    else:
-        error_msg = f"Connection failed with code {rc}"
-        logging.error(error_msg)
-        raise MQTTConnectionError(error_msg)
+        # This method is called when a message is received on the MQTT topic
+        message = message.payload.decode('utf-8')
+        logging.info("Received MQTT message: %s", message)
 
+        try:
+            # Send the message to the specified chat ID
+            send_text = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&parse_mode=Markdown&text=' + message
+            response = requests.get(send_text)
+            logging.debug(response)
 
-def on_message(client, userdata, message) -> None:
-    """
-    Parse the incoming messages.
-    """
-    
-    _ = client
-    _ = userdata
-    logging.info("Received message: %s", message.payload.decode())
-
-
-# MQTT client setup
-mqttc = mqtt.Client(CLIENT_ID)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-
-
-def mqtt_client():
-    """" All application has its initialization from here """
-    logging.info('Main application is running!')
-
-    try:
-
-        mqttc.connect(BROKER_ADDRESS, PORT)
-        mqttc.loop_start()
-
-        while True:
-            pass
-
-            # message = "message to publish!"
-            # mqttc.publish(TOPIC, message)
-            # logging.info("Published message: %s", message)
-
-    except mqtt as e:
-        logging.exception("An error occurred: %s", str(e), exc_info=False)
-        traceback.print_exc()
-
-    finally:
-        mqttc.disconnect()
-        logging.info("Disconnected from MQTT broker")
+        except Exception as e:
+            logging.error(f"Error sending message to Telegram: {str(e)}", exc_info=False)
